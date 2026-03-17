d21caadfa3e613b7072920068e72214bf28737c0
Rijwanool karim <rijwanoolkarim143r@email.com>
2026-03-17 17:01:18 +0530

    change batch size

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ CONFIG = {
     "data_files": [
         "data/raw/CIC-IDS-2017_fin_capped.csv",
         "data/raw/full_unsw.csv",
-        # "data/raw/NF_ToN_IoT_v3_clean.csv"
+        "data/raw/NF_ToN_IoT_v3_clean.csv"
     ],
     "label_names": ["Label", "label", "attack_cat", "class"],
 
-    "batch_size": 1024,
+    "batch_size": 2048,
     "epochs": 20,
     "learning_rate": 0.0001,
     "weight_decay": 1e-4,
